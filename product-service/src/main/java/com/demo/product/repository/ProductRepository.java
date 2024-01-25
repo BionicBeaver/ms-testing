07e1ce28fdea3287c44b3a9ef99b4d070a362c7f
beaver <drago-2707@hotmail.com>
2024-01-24 23:40:22 -0600

    Add product service

---
+++ product-service/src/main/java/com/demo/product/repository/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.demo.product.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.demo.product.entity.Category;
+import com.demo.product.entity.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    public List<Product> findByCategory(Category category);
+}
