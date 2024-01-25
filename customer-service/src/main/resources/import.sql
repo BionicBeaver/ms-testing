f2ca21f12577a33da44adf5aa2dfce6290f6db66
beaver <drago-2707@hotmail.com>
2024-01-24 23:57:49 -0600

    Add customer service

---
+++ customer-service/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tbl_region (name) VALUES ('Sudamerica');
+INSERT INTO tbl_region (name) VALUES ('Centroamerica');
+INSERT INTO tbl_region (name) VALUES ('Norteamerica');
+INSERT INTO tbl_region (name) VALUES ('Europa');
+INSERT INTO tbl_region (name) VALUES ('Asia');
+INSERT INTO tbl_region (name) VALUES ('Africa');
+INSERT INTO tbl_region (name) VALUES ('Oceania');
+INSERT INTO tbl_region (name) VALUES ('Antartida');
+
+INSERT INTO tbl_customer (number_id,first_name,last_name , email, photo_url, region_id, state) VALUES('32404580', 'Andrés', 'Guzmán', 'profesor@bolsadeideas.com','',1,'CREATED');
